459ccb334f4e30c4c8c4fcacc0af0ff946a94ff0
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-06-06 01:08:27 +0000

    add likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_06_004829) do
+ActiveRecord::Schema.define(version: 2023_06_06_005413) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "fan_id"
+    t.integer "photo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "caption"
