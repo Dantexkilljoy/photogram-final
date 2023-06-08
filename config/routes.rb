133e9829a3b193102901e70d6cb106e4fb8aee6e
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-06-08 19:49:23 +0000

    fiexed user nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Routes for the Comment resource:
 
+  get("/users", { controller: "user", action: "index" })
+
   get("/", { controller: "user", action: "index" })
 
   # CREATE
