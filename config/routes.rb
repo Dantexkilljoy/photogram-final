7edb24bde6577c351063c54b1262934b80cd74e8
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-06-13 19:28:51 +0000

    fixed liked users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-  # Routes for the Comment resource:
-
   get("/users/:path_id", { controller: "user", action: "show" })
 
   get("/users", { controller: "user", action: "index" })
 
   get("/", { controller: "user", action: "index" })
 
+  get("/users/:path_id/liked_photos", { controller: "user", action: "likes" })
+
   # CREATE
   post("/insert_comment", { :controller => "comments", :action => "create" })
 
-  # READ
-  get("/comments", { :controller => "comments", :action => "index" })
-
-  get("/comments/:path_id", { :controller => "comments", :action => "show" })
+  post("/modify_user/:path_id", { :controller => "user", :action => "update" })
 
   # UPDATE
 
@@ -29,11 +26,6 @@ Rails.application.routes.draw do
   # CREATE
   post("/insert_follow_request", { :controller => "follow_requests", :action => "create" })
 
-  # READ
-  get("/follow_requests", { :controller => "follow_requests", :action => "index" })
-
-  get("/follow_requests/:path_id", { :controller => "follow_requests", :action => "show" })
-
   # UPDATE
 
   post("/modify_follow_request/:path_id", { :controller => "follow_requests", :action => "update" })
@@ -47,11 +39,6 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_like", { :controller => "likes", :action => "create" })
-
-  # READ
-  get("/likes", { :controller => "likes", :action => "index" })
-
-  get("/likes/:path_id", { :controller => "likes", :action => "show" })
 
   # UPDATE
 
